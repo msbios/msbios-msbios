90e31adf2480acf0e8e529ad803429d920faba11
Judzhin Miles <info@msbios.com>
2017-07-10 10:23:56 +0300

    Intermedaite commit

---
+++ src/ModuleInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Interface ModuleInterface
+ * @package MSBios
+ */
+interface ModuleInterface extends ConfigProviderInterface
+{
+
+}
---
+++ src/Version.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios;
+
+/**
+ * Class Version
+ * @package MSBios
+ */
+abstract class Version
+{
+    /** @const NUMBER */
+    const NUMBER = '0.0.1';
+}
