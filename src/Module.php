24fc90f18acce199ca01b31c67dcac13c2b0e1c4
Judzhin Miles <info@msbios.com>
2018-11-08 22:31:25 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@ namespace MSBios;
 class Module implements ModuleInterface, ModuleAwareInterface, AutoloaderAwareInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.7';
+    const VERSION = '1.0.8';
 
     use ModuleAwareTrait;
     use AutoloaderAwareTrait;
