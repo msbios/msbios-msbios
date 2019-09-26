dc6d9b866c40792ae5f0c5e2c2fccea2344f8536
Judzhin Miles <info@msbios.com>
2019-09-26 20:27:27 +0300

    Lazy Composer MSBios Commit Script

---
+++ src/ConfigProvider.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+
+namespace MSBios;
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+/**
+ * Class ConfigProvider
+ *
+ * Provide base configuration for using the component.
+ *
+ * @package MSBios
+ */
+class ConfigProvider
+{
+    /** @const SERVICE_MANAGER  */
+    const SERVICE_MANAGER = 'service_manager';
+
+    /** @const ROUTE_MANAGER  */
+    const ROUTE_MANAGER = 'route_manager';
+
+    /** @const ROUTER  */
+    const ROUTER = 'router';
+
+    /**
+     * Provide default configuration.
+     *
+     * @return array
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencyConfig(),
+        ];
+    }
+
+    /**
+     * Provide default container dependency configuration.
+     *
+     * @return array
+     */
+    public function getDependencyConfig(): array
+    {
+        return [
+            'abstract_factories' => [
+                ModuleAbstractFactory::class =>
+                    new ModuleAbstractFactory
+            ],
+            'factories' => [
+                ListenerAggregate::class =>
+                    InvokableFactory::class
+            ]
+        ];
+    }
+}
