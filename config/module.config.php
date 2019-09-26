2947634f169badff9df23d23246e47c9063a81c9
Judzhin Miles <info@msbios.com>
2019-09-26 20:41:38 +0300

    Lazy Composer MSBios Commit Script

--- config/module.config.php
+++ config/module.config.php
@@ -5,8 +5,26 @@
  */
 namespace MSBios;
 
+use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
+
+    'service_manager' => [
+        'abstract_factories' => [
+            ModuleAbstractFactory::class =>
+                new ModuleAbstractFactory
+        ],
+        'factories' => [
+            ListenerAggregate::class =>
+                InvokableFactory::class
+        ]
+    ],
+
     Module::class => [
         // ...
     ],
+
+    'listeners' => [
+        ListenerAggregate::class
+    ]
 ];
